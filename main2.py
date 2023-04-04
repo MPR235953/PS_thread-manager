2a5fa223eb22c07800c9c2a6c88a253098b599b5
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-04-04 22:04:06 +0200

    feat: multiple threda start/stop by checkboxes

--- main2.py
+++ main2.py
@@ -9,7 +9,6 @@ from PyQt5.QtWidgets import *
 
 class Worker(QObject):
     sig_update = pyqtSignal(str, name='sig_update')
-    sig_finished = pyqtSignal(int, name='sig_finished')
     def __init__(self, id, parent=None):
         super(Worker, self).__init__(parent)
 
@@ -17,6 +16,7 @@ class Worker(QObject):
         self.pointer = 0
         self.paused = True
         self.quit = False
+        self.finished = False
 
         self.thread = threading.Thread(target=self.task)
         self.lock = threading.Lock()
@@ -29,9 +29,9 @@ class Worker(QObject):
                 if char == 'Z': break
                 self.sig_update.emit(char + str(self.id) + '\n')
                 self.pointer += 1
-                time.sleep(0.5)
+                time.sleep(0.1)
             else: self.lock.acquire()
-        self.sig_finished.emit(self.id)
+        self.finished = True
 
     def pause(self):
         self.paused = True
@@ -71,11 +71,15 @@ class MainWindow(QMainWindow):
         self.lb_thread_no.setGeometry(QRect(130, 10, 131, 17))
         self.lb_thread_no.setObjectName("lb_thread_no")
 
-        self.sb_thread_no = QSpinBox(self)
-        self.sb_thread_no.setGeometry(QRect(130, 40, 104, 70))
-        self.sb_thread_no.setObjectName("sb_thread_no")
-        self.sb_thread_no.setMinimum(1)
-        self.sb_thread_no.setMaximum(10)
+        self.checkboxes = []
+        x, y = 0, 0
+        for i in range(10):
+            if i == 5: x = 40; y = 0;
+            self.checkboxes.append(QCheckBox(self))
+            self.checkboxes[-1].setCheckable(True)
+            self.checkboxes[-1].setText(str(i + 1))
+            self.checkboxes[-1].setGeometry(QRect(130 + x, 30 + y, 40, 20))
+            y += 20
 
         self.te_terminal = QTextEdit(self)
         self.te_terminal.setGeometry(QRect(20, 130, 211, 421))
@@ -89,51 +93,55 @@ class MainWindow(QMainWindow):
             self.init_worker(id=i+1)
 
     def update_terminal(self, msg: str):
+        ''' method to updating terminal '''
         self.terminal_text += msg
         self.te_terminal.setText(self.terminal_text)
         self.te_terminal.verticalScrollBar().setValue(self.te_terminal.verticalScrollBar().maximum())
 
     def init_worker(self, id):
+        ''' method to initialize worker object with a given id '''
         worker = Worker(id)
         worker.sig_update.connect(self.update_terminal)
-        worker.sig_finished.connect(self.clear_worker)
         worker.start()
         self.workers.append(worker)
-        self.update_terminal("Worker " + str(id) + " initialized" + '\n')
+        self.update_terminal("Worker " + str(id) + " waiting" + '\n')
 
-    def find_worker(self, raw_id) -> Worker | None:
-        try:
-            w_id = int(raw_id)
-            for worker in self.workers:
-                if worker.id == w_id:
-                    return worker
-        except:
-            return None
+    def pause_worker(self) -> None:
+        ''' resume threads that checkboxes are checked and put info on terminal '''
+        for checkbox in self.checkboxes:
+            if checkbox.isChecked():
+                w_id = int(checkbox.text())
+                if self.workers[w_id - 1].finished:
+                    self.update_terminal("Worker " + str(w_id) + " was finished" + '\n')
+                    continue
+                if self.workers[w_id - 1].paused:
+                    self.update_terminal("Worker " + str(w_id) + " already paused" + '\n')
+                else:
+                    self.workers[w_id - 1].pause()
+                    self.update_terminal("Worker " + str(w_id) + " paused" + '\n')
 
-    def pause_worker(self):
-        raw_id = self.sb_thread_no.value()
-        worker = self.find_worker(raw_id)
-        if worker:
-            worker.pause()
-            self.update_terminal("Worker " + str(raw_id) + " paused" + '\n')
+    def resume_worker(self) -> None:
+        ''' resume threads that checkboxes are checked and put info on terminal '''
+        for checkbox in self.checkboxes:
+            if checkbox.isChecked():
+                w_id = int(checkbox.text())
+                if self.workers[w_id - 1].finished:
+                    self.update_terminal("Worker " + str(w_id) + " was finished" + '\n')
+                    continue
+                if not self.workers[w_id - 1].paused:
+                    self.update_terminal("Worker " + str(w_id) + " already running" + '\n')
+                else:
+                    self.workers[w_id - 1].resume()
+                    self.update_terminal("Worker " + str(w_id) + " resumed" + '\n')
 
-    def resume_worker(self):
-        raw_id = self.sb_thread_no.value()
-        worker = self.find_worker(raw_id)
-        if worker:
-            worker.resume()
-            self.update_terminal("Worker " + str(raw_id) + " resumed" + '\n')
-        else:
-            self.init_worker(int(raw_id))
+    def quit_app(self) -> None:
+        ''' close app by button '''
+        for worker in self.workers:
+            worker.finish()
+        QApplication.quit()
 
-    def clear_worker(self, i):
-        worker = self.find_worker(i)
-        worker.finish()
-        index = self.workers.index(worker)
-        del self.workers[index]
-        self.update_terminal("Worker " + str(i) + " finished" + '\n')
-
-    def quit_app(self):
+    def closeEvent(self, event) -> None:
+        ''' close app by X '''
         for worker in self.workers:
             worker.finish()
         QApplication.quit()
